662fbfb0f3486e89151a89e4bc62b786018829fd
kulklex <kulklex@gmail.com>
2024-01-18 14:10:16 +0000

    Minor changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const fs = require("fs");
-const ObjectID = require('mongodb').ObjectId;
+const ObjectID = require("mongodb").ObjectId;
 
 // Allow use from any url
 app.use(cors());
@@ -38,23 +38,18 @@ app.use((req, res, next) => {
   });
 });
 
-
-
 // use json data
 app.use(express.json());
 
 // connect to the database
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://kulklex:PHmrdQlNT8qwwClK@cluster0.z1hfgdd.mongodb.net/Kulklex"
-
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://kulklex:PHmrdQlNT8qwwClK@cluster0.z1hfgdd.mongodb.net/Kulklex";
 
 let db;
 MongoClient.connect(uri, (err, client) => {
-    db = client.db('Kulklex')
-})
-
-
-
+  db = client.db("Kulklex");
+});
 
 // Test api is working
 app.get("/", (req, res, next) => {
@@ -62,59 +57,68 @@ app.get("/", (req, res, next) => {
 });
 
 // Get all lessons
-app.get('/lessons', (req, res, next) => {
-    db.collection('lessons').find({}).toArray((err, results) => {
-        if (err) {
-            console.error(err);
-            return next(err);
-        }
-        res.json(results);
+app.get("/lessons", (req, res, next) => {
+  db.collection("lessons")
+    .find({})
+    .toArray((err, results) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      res.json(results);
     });
 });
 
 // Update lesson spaces
-app.put('/lessons/:lessonID', (req, res, next) => {
-
-    req.body.forEach((item) => {
-        let filter = { _id: new ObjectID(item._id) }
-        let newValue = { $set: {spaces: item.spaces} }
-        let options = { safe: true, multi: false }
-        db.collection('lessons').updateOne(filter, newValue, options, (err, result) => {
-            if (err) return next(err)
-        })
-    });
-    res.send({msg: "Spaces updated!"})
-})
+app.put("/lessons/:lessonID", (req, res, next) => {
+  req.body.forEach((item) => {
+    let filter = { _id: new ObjectID(item._id) };
+    let newValue = { $set: { spaces: item.spaces } };
+    let options = { safe: true, multi: false };
+    db.collection("lessons").updateOne(
+      filter,
+      newValue,
+      options,
+      (err, result) => {
+        if (err) return next(err);
+      }
+    );
+  });
+  res.send({ msg: "Spaces updated!" });
+});
 
 // Add a new order
 app.post("/orders", (req, res, next) => {
-    let order = req.body
-    req.collection.insertOne(order, (err, result) => {
-        if (err) return next(err)
-        res.send({msg: "Order added successfully"})
-    })
-})
+  let order = req.body;
+  db.collection("orders").insertOne(order, (err, result) => {
+    if (err) return next(err);
+    res.send({ msg: "Order added successfully" });
+  });
+});
 
 // Search route
-app.get('/lessons/search', (req, res) => {
-    let searchQuery = req.query.filter
-    req.collection.find({}).toArray((err, results) => {
-        if (err) return next(err)
-        let filteredList = results.filter((lesson) => {
-            return lesson.subject.toLowerCase().match(searchQuery.toLowerCase()) || lesson.location.toLowerCase().match(searchQuery.toLowerCase())
-        });  
-        res.send(filteredList)
-    })
-})
-
+app.get("/lessons/search", (req, res) => {
+  let searchQuery = req.query.filter;
+  db.collection("lessons")
+    .find({})
+    .toArray((err, results) => {
+      if (err) return next(err);
+      let filteredList = results.filter((lesson) => {
+        return (
+          lesson.subject.toLowerCase().match(searchQuery.toLowerCase()) ||
+          lesson.location.toLowerCase().match(searchQuery.toLowerCase())
+        );
+      });
+      res.send(filteredList);
+    });
+});
 
 // Error Handling
 app.use((req, res) => {
-    res.status(404)
-    res.send("File not found")
-})
-
+  res.status(404);
+  res.send("File not found");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
